157aefcb11a351733e43e07ae9c336f2ef9662ed
Imran Qasim <imran_qasim@hotmail.com>
2018-07-11 18:33:24 +0500

    Part 08

---
+++ app/Http/Requests/StoreProduct.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreProduct extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+           'title'=>'required',
+           'slug' => 'required',
+           'description'=>'required',
+           'thumbnail' => 'required|mimes:jpeg,bmp,png|max:2048',
+           'status' => 'required|numeric',
+           'category_id' => 'required',
+           'price' => 'required|numeric'
+        ];
+    }
+
+}
---
+++ database/migrations/2018_07_09_122356_update_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+           $table->boolean('featured')->default(0)->after('options');
+           $table->boolean('status')->default(0)->after('featured');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('featured');
+            $table->dropColumn('status');
+        });
+    }
+}
