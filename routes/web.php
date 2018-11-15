fc629767f030f357e7819a5a0b441c53e32e2964
Imran Qasim <imran_qasim@hotmail.com>
2018-11-15 19:08:01 +0500

    updated laravel 5.7 on ecom website

--- routes/web.php
+++ routes/web.php
@@ -14,49 +14,47 @@ use Illuminate\Support\Facades\DB;
 */
 
 Route::get('/', function () {
-    $title = DB::table('products')->select('title')->get();
-    dd($title);
+    return view('welcome');
 });
 Route::view('/checkout', 'products.checkout');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['as'=>'products.', 'prefix'=>'products'], function(){
-	Route::get('/', 'ProductController@show')->name('all');
-	Route::get('/{product}', 'ProductController@single')->name('single');
-	Route::get('/addToCart/{product}', 'ProductController@addToCart')->name('addToCart');
+Route::group(['as'=>'products.', 'prefix'=>'products'], function () {
+    Route::get('/', 'ProductController@show')->name('all');
+    Route::get('/{product}', 'ProductController@single')->name('single');
+    Route::get('/addToCart/{product}', 'ProductController@addToCart')->name('addToCart');
 });
 
-Route::group(['as'=>'cart.', 'prefix'=>'cart'], function(){
-	Route::get('/', 'ProductController@cart')->name('all');
-	Route::post('/remove/{product}', 'ProductController@removeProduct')->name('remove');
-	Route::post('/update/{product}', 'ProductController@updateProduct')->name('update');
+Route::group(['as'=>'cart.', 'prefix'=>'cart'], function () {
+    Route::get('/', 'ProductController@cart')->name('all');
+    Route::post('/remove/{product}', 'ProductController@removeProduct')->name('remove');
+    Route::post('/update/{product}', 'ProductController@updateProduct')->name('update');
 });
 
-Route::group(['as'=>'admin.', 'middleware'=>['auth','admin'], 'prefix'=>'admin'], function(){
-	
-	Route::get('/', 'AdminController@dashboard');
-	Route::get('category/{category}/remove','CategoryController@remove')->name('category.remove');
-	Route::get('category/trash', 'CategoryController@trash')->name('category.trash');
-	Route::get('category/recover/{id}', 'CategoryController@recoverCat')->name('category.recover');
+Route::group(['as'=>'admin.', 'middleware'=>['auth','admin'], 'prefix'=>'admin'], function () {
+    Route::get('/', 'AdminController@dashboard');
+    Route::get('category/{category}/remove', 'CategoryController@remove')->name('category.remove');
+    Route::get('category/trash', 'CategoryController@trash')->name('category.trash');
+    Route::get('category/recover/{id}', 'CategoryController@recoverCat')->name('category.recover');
 
-	Route::get('product/{product}/remove','ProductController@remove')->name('product.remove');
-	Route::get('product/trash', 'ProductController@trash')->name('product.trash');
-	Route::get('product/recover/{id}', 'ProductController@recoverProduct')->name('product.recover');
-	
-	Route::view('product/extras', 'admin.partials.extras')->name('product.extras');
+    Route::get('product/{product}/remove', 'ProductController@remove')->name('product.remove');
+    Route::get('product/trash', 'ProductController@trash')->name('product.trash');
+    Route::get('product/recover/{id}', 'ProductController@recoverProduct')->name('product.recover');
+    
+    Route::view('product/extras', 'admin.partials.extras')->name('product.extras');
 
-	Route::get('profile/{profile}/remove','ProfileController@remove')->name('profile.remove');
-	Route::get('profile/trash', 'ProfileController@trash')->name('profile.trash');
-	Route::get('profile/recover/{id}', 'ProfileController@recoverProduct')->name('profile.recover');
-	Route::view('profile/roles', 'admin.partials.extras')->name('profile.extras');	
+    Route::get('profile/{profile}/remove', 'ProfileController@remove')->name('profile.remove');
+    Route::get('profile/trash', 'ProfileController@trash')->name('profile.trash');
+    Route::get('profile/recover/{id}', 'ProfileController@recoverProduct')->name('profile.recover');
+    Route::view('profile/roles', 'admin.partials.extras')->name('profile.extras');
 
-	Route::get('profile/states/{id?}', 'ProfileController@getStates')->name('profile.states');
-	Route::get('profile/cities/{id?}', 'ProfileController@getCities')->name('profile.cities');
-	
-	Route::get('dashboard', 'AdminController@dashboard')->name('dashboard');
-	Route::resource('product', 'ProductController');
-	Route::resource('category', 'CategoryController');
-	Route::resource('profile','ProfileController');
+    Route::get('profile/states/{id?}', 'ProfileController@getStates')->name('profile.states');
+    Route::get('profile/cities/{id?}', 'ProfileController@getCities')->name('profile.cities');
+    
+    Route::get('dashboard', 'AdminController@dashboard')->name('dashboard');
+    Route::resource('product', 'ProductController');
+    Route::resource('category', 'CategoryController');
+    Route::resource('profile', 'ProfileController');
 });
